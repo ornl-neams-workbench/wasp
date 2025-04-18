75d6c8805fee18eeffbf09f16c8dc6a039b29dfc
Robert A. Lefebvre <lefebvrera@ornl.gov>
2025-04-17 23:51:32 -0400

    Add missing Grid serialization

--- waspplot/CustomPlotSerialization.h
+++ waspplot/CustomPlotSerialization.h
@@ -33,6 +33,26 @@ DataObject WASP_PUBLIC serializeTitle(const CustomPlot::Title& title);
 DataObject WASP_PUBLIC serializeLegend(const CustomPlot::Legend& legend);
 
 /**
+ * @brief Serializes a Brush object to a DataObject.
+ * @param brush The CustomPlot::Brush to serialize.
+ * @return DataObject representation of the brush.
+ */
+DataObject serializeBrush(const CustomPlot::Brush& brush);
+
+/**
+ * @brief Serializes a Pen object to a DataObject.
+ * @param pen The CustomPlot::Pen to serialize.
+ * @return DataObject representation of the pen.
+ */
+DataObject serializePen(const CustomPlot::Pen& pen);
+
+/**
+ * @brief Serializes a Grid object to a DataObject.
+ * @param grid The CustomPlot::Grid to serialize.
+ * @return DataObject representation of the grid.
+ */
+DataObject serializeGrid(const CustomPlot::Grid& grid);
+/**
  * @brief Serializes a CustomPlot::Axis object into a DataObject.
  */
 DataObject WASP_PUBLIC serializeAxis(const CustomPlot::Axis& axis);
@@ -66,6 +86,27 @@ CustomPlot::Title WASP_PUBLIC deserializeTitle(const DataObject& obj);
  * @brief Deserializes a DataObject into a CustomPlot::Legend.
  */
 CustomPlot::Legend WASP_PUBLIC deserializeLegend(const DataObject& obj);
+
+/**
+ * @brief Deserializes a Brush object from a DataObject.
+ * @param obj The DataObject containing brush data.
+ * @return Deserialized CustomPlot::Brush.
+ */
+CustomPlot::Brush deserializeBrush(const DataObject& obj);
+
+/**
+ * @brief Deserializes a Pen object from a DataObject.
+ * @param obj The DataObject containing pen data.
+ * @return Deserialized CustomPlot::Pen.
+ */
+CustomPlot::Pen deserializePen(const DataObject& obj);
+
+/**
+ * @brief Deserializes a Grid object from a DataObject.
+ * @param obj The DataObject containing grid data.
+ * @return Deserialized CustomPlot::Grid.
+ */
+CustomPlot::Grid deserializeGrid(const DataObject& obj);
 
 /**
  * @brief Deserializes a DataObject into a CustomPlot::Axis.

01a69941547e1b27783058f985b09933630233b9
Henderson, Shane <hendersonsc@ornl.gov>
2025-08-15 15:46:41 +0000

    Add cell density to mcnpmaterials

--- wasputils/mcnpimaterials.cpp
+++ wasputils/mcnpimaterials.cpp
@@ -3,14 +3,23 @@
 #include <math.h>
 #include <iostream>
 #include <fstream>
+#include <map>
 #include "waspcore/version.h"
 #include "waspcore/wasp_bug.h"
 #include "waspcore/Object.h"
-
+#include "waspjson/JSONObjectParser.hpp"
 #include "waspmcnpi/MCNPInterpreter.h"
 #include "waspmcnpi/Model.h"
 
 using namespace wasp;
+
+void dump_errors(DataArray& da_error)
+{
+    if(!da_error.empty())
+    {
+        da_error.format_json(std::cout);
+    }
+}
 
 int main(int argc, char* argv[])
 {
@@ -22,13 +31,14 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    if (argc == 1)
+    if (argc == 2)
     {
         std::cout << "Workbench Analysis Sequence Processor (WASP)" << std::endl
                   << argv[0] << " : MCNP to JSON (materials) Converter"
                   << std::endl;
-        std::cout << " Usage : " << argv[0] << " path/to/MCNP/formatted/input"
-                  << std::endl;
+        std::cout << " Usage : " << argv[0] << " path/to/MCNP/formatted/input path/to/zaid-molar-masses.json" << std::endl
+                  << " zaid-molar-masses.json format requirement is {\"zaid\": mass, ...}. " << std::endl 
+                  << " For example {\"1001\": 1.007830023765564,...} " << std::endl;
         std::cout << " Usage : " << argv[0]
                   << " --version\t(print version info)" << std::endl;
         return 1;
@@ -36,34 +46,52 @@ int main(int argc, char* argv[])
 
 
     DataArray da_error;
-    for (int j = 1; j < argc; ++j)
-    {
-        std::stringstream        errors;
 
-        // First attempt to parse the document
-        DefaultMCNPInterpreter interpreter(errors);
-        bool parsed = interpreter.parseFile(argv[j]);                
-        if (!parsed)
-        {
-            da_error.push_back(errors.str());
-            continue;
-        }
-  
-        // Next try to construct the MCNP model
-        mcnpi::Model model;
-        bool success = model.build_limited(interpreter.root(), errors);
-        if (!success)
-        {
-            da_error.push_back(errors.str());
-            continue;
-        }
-        // Output the materials in JSON format
-        model.describe_materials_json(std::cout);       
-    }
-    if(!da_error.empty())
+    std::stringstream        errors;
+    DataObject::SP zaid_molar_masses_json; 
+    std::ifstream masses_stream(argv[2]);
+    if (!masses_stream.good())
     {
-        da_error.format_json(std::cout);
-        return -1;
+        std::cerr << "Unablet to read " << argv[2] << "!" << std::endl;
+        return 1;
     }
+
+    // Attempt to interpreter the molar masses
+    JSONObjectParser generator(zaid_molar_masses_json, masses_stream, errors, nullptr);
+
+    if (generator.parse() != 0)
+    {
+        da_error.push_back(errors.str());
+        dump_errors(da_error); return -1;
+    }
+
+    // Convert json object to map<int, double>
+    std::map<int, double> molar_masses; 
+    for (auto itr = zaid_molar_masses_json->begin(); itr != zaid_molar_masses_json->end(); itr++)
+    {
+        molar_masses[std::stoi(itr->first)] = itr->second.to_double();
+    }
+
+    // Attempt to parse the document
+    DefaultMCNPInterpreter interpreter(errors);
+    bool parsed = interpreter.parseFile(argv[1]);                
+    if (!parsed)
+    {
+        da_error.push_back(errors.str());
+        dump_errors(da_error); return -1;
+    }
+
+    // Next try to construct the MCNP model
+    mcnpi::Model model;
+    model.set_zaid_relative_atomic_mass_map(&molar_masses);
+    bool success = model.build(interpreter.root(), errors);
+    if (!success)
+    {
+        da_error.push_back(errors.str());
+        dump_errors(da_error); return -1;
+    }
+    // Output the materials in JSON format
+    model.describe_materials_json(std::cout);       
+    
     return 0;
 }
